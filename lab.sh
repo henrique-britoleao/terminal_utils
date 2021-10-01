cc1dcf538768b4acd65f70a82b498f8452db36f5
henrique-britoleao <hbl451@gmail.com>
2021-10-01 10:20:32 +0200

    Create first version of lab command

---
+++ lab.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function create_project () {
+    local projects_dir=~/Documents/projects/$1
+    mkdir -p ${projects_dir}/bin && cd $projects_dir
+    touch .gitignore && touch README.md
+    git init
+    code $projects_dir
+}
+
+command=$1
+
+case $command in
+    new) 
+        shift # shift arguments to ignore command
+        while getopts n: flag
+        do
+            case "${flag}" in
+                n) 
+                    name=${OPTARG}
+                    create_project $name;;
+            esac
+        done;;
+    
+    open) echo "open";;
+    *) echo "lab: '$command' command not found"
+esac
