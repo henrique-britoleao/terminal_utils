3ae4677990d874f1e42f8f5878e0128c9430ae1c
henrique-britoleao <hbl451@gmail.com>
2021-10-01 10:56:53 +0200

    Added long option

--- lab.sh
+++ lab.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function create_project () {
-    local projects_dir=~/Documents/projects/$1
+    local projects_dir=~/Documents/projects/$1 # TODO: Add config to change root dir 
     mkdir -p ${projects_dir}/bin 
-    cat ./ignore.template >> ${projects_dir}/.gitignore
+    cat ./ignore.template > ${projects_dir}/.gitignore
     cd $projects_dir && touch README.md
     git init
     code $projects_dir
@@ -14,6 +14,17 @@ command=$1
 case $command in
     new) 
         shift # shift arguments to ignore command
+        
+        # Transform long options to short ones
+        for arg in "$@"
+        do
+            shift
+            case "$arg" in
+                "--name") set -- "$@" "-n" ;;
+                "--"*)    echo "illegal option" "$arg"; exit 1;;
+            esac
+        done
+
         while getopts n: flag
         do
             case "${flag}" in
@@ -24,5 +35,4 @@ case $command in
         done;;
     
     open) echo "open";;
-    *) echo "lab: '$command' command not found"
 esac
